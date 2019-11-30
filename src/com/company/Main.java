4efa59a6890ed495308826b17a7ad6041452a985
Jessie Fernandez <jessievf@gmail.com>
2019-11-29 22:15:22 -0800

    added more tests

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,17 +11,20 @@ public class Main {
         DLL.addFirst( 5 );
         DLL.addFirst( 2);
         DLL.addFirst( 1 );
+        DLL.addFirst(5);
         DLL.addLast(6);
         DLL.addLast(7);
         DLL.addLast(8);
+        DLL.addLast(9);
 
 
 
         System.out.println(DLL.size()); //get size
         System.out.println(DLL.getFirst()); //get first element
         System.out.println(DLL.getLast());  //get last element
-        DLL.printHead();    //should print out 1
-        DLL.printTail();    //should print out 8
+        DLL.printHead();    //should print out 5
+        DLL.printTail();    //should print out 9
+
 
 
 
